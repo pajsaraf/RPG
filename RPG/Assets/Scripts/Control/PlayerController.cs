10200747a1ecdf1e9ab3e0bde66cb168df5c780b
paj <pajsaraf@gmail.com>
2021-12-02 14:00:34 -0500

    27 combat step 1
    debug to interact with a target

--- RPG/Assets/Scripts/Control/PlayerController.cs
+++ RPG/Assets/Scripts/Control/PlayerController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using System;
+using RPG.Combat;
 
 namespace RPG.Control   //creating a name space to control dependencies
 {
@@ -9,6 +11,28 @@ namespace RPG.Control   //creating a name space to control dependencies
     {
 
         private void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());   // shoot raycast to look for a target in priority and if none found move instead
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if(Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+
+        }
+
+        private void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
@@ -18,15 +42,19 @@ namespace RPG.Control   //creating a name space to control dependencies
 
         private void MoveToCurser()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //extracted Camera.main.ScreenPointToRay(Input.mousePosition)
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
         }
 
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
     }
 }
 
