34878f9ed933ad249cebb08c5a47546c7e2dee2c
paj <pajsaraf@gmail.com>
2021-12-06 19:00:12 -0500

    29
    stop when in range of enemy - some errors need fixing

--- RPG/Assets/Scripts/Combat/Fighter.cs
+++ RPG/Assets/Scripts/Combat/Fighter.cs
@@ -1,13 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using RPG.Movement;
 
-namespace RPG.Combat{
+namespace RPG.Combat
+{
     public class Fighter : MonoBehaviour
-    {
-        public void Attack(CombatTarget target)
+    {  
+        [SerializeField] float weaponRange = 2f;
+        Transform target;   
+
+        private void Update() 
         {
-            print("attack");
+            bool isInRange = Vector3.Distance(transform.position, target.position) < weaponRange;
+
+            if(target !=null && !isInRange)
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Stop();
+            }
+        }
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
         }
 
     }
--- RPG/Assets/Scripts/Movement/Mover.cs
+++ RPG/Assets/Scripts/Movement/Mover.cs
@@ -8,9 +8,13 @@ namespace RPG.Movement
     public class Mover : MonoBehaviour
     {
         [SerializeField] Transform target;
-
+        NavMeshAgent navMeshAgent;
         Ray lastRay;  //last ray shot at screen
 
+        private void Start() 
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
 
         // Update is called once per frame
         void Update()
@@ -22,12 +26,18 @@ namespace RPG.Movement
 
         public void MoveTo(Vector3 destination)
         {
-            GetComponent<NavMeshAgent>().destination = destination;
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Stop()
+        {
+            navMeshAgent.isStopped = true;
         }
 
         private void UpdateAnimator()
         {
-            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;  // get the speed from navmesh component
+            Vector3 velocity = navMeshAgent.velocity;  // get the speed from navmesh component
             Vector3 localVelocity = transform.InverseTransformDirection(velocity);  //make velocity variable into  a local variable to find just the speed and not the global positioning
             float speed = localVelocity.z;
             GetComponent<Animator>().SetFloat("forwardSpeed", speed);
