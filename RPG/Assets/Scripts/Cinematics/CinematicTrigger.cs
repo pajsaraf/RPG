7bb355bd2e828e4c5f324b7a9736d62c70fe6183
paj <pajsaraf@gmail.com>
2022-01-11 10:46:33 -0500

    65
    cinematic and trigger setup. still have some issues to fix

---
+++ RPG/Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;  //Playables Director - cinematics
+
+namespace RPG.Cinematics
+{
+   
+
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool alreadyTriggered = false;
+
+        private void OnTriggerEnter(Collider other) 
+        {
+            if (alreadyTriggered == false && other.gameObject.tag == "Player" )
+            {
+                alreadyTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+            }
+        }
+    }
+}
