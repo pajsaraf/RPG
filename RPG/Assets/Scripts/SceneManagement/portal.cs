7ca9bc6bd3e13c602da50f43cd904fac1b2d5418
paj <pajsaraf@gmail.com>
2022-01-12 19:10:37 -0500

    70
    created a spawning teleportal called Portal to go between scenes, also created a second scene as a sort of interior

---
+++ RPG/Assets/Scripts/SceneManagement/portal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class portal : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad = -1;  // this will let me pick the scene at the editor level AND give errors if we dont put anything
+        // need to go to editor File >> Build settings and look at the index of the scenes, then use that number in the editor 
+        private void OnTriggerEnter(Collider other) 
+        {
+            //print("Going Inside");
+            if (other.tag == "Player")
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+
+}
